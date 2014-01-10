c60f92b144edf4a76f3e96469b32c9819db37cd0
Dave Amenta <dave@daveamenta.com>
2014-01-10 12:48:29 -0800

    init

---
+++ Taskbar.psm1
@@ -0,0 +1,28 @@
+﻿
+function Get-ConsoleWindowHWND {
+    add-type -MemberDefinition @"
+[DllImport("KERNEL32.DLL")]
+public static extern int GetConsoleWindow();
+"@ -Namespace $null -Name GCW
+    [GCW]::GetConsoleWindow()
+}
+
+function Set-PowerShellAppId($AppId) {
+    if (!$IsTaskHost -and !$IsRemote -and !$IsBackground) {
+        try {
+            add-type -Path $PsScriptRoot\Microsoft.WindowsAPICodePack.Shell.dll
+            $tb = [Microsoft.WindowsAPICodePack.Taskbar.TaskbarManager]::Instance
+            $tb.SetApplicationIdForSpecificWindow((Get-ConsoleWindowHWND), $AppId)
+        } catch [exception] { msg "Didn't update taskbar" }
+    }
+}
+
+function Set-TaskbarOverlayIcon($icoPath, $accText) {
+    if (!$IsTaskHost -and !$IsRemote -and !$IsBackground) {
+        try {
+            add-type -Path $PsScriptRoot\Microsoft.WindowsAPICodePack.Shell.dll
+            $tb = [Microsoft.WindowsAPICodePack.Taskbar.TaskbarManager]::Instance
+            $tb.SetOverlayIcon([System.Drawing.Icon]$icoPath, $accText)
+        } catch [exception] { msg "Didn't update taskbar" }
+    }
+}
